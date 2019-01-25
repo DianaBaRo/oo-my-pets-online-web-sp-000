ae5842c9ff94559b1ce605d0e3dbe2a14988075c
Diana Badas <diana.badas@gmail.com>
2019-01-25 21:18:58 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,6 @@
 class Owner
 
-  attr_accessor :name, :pets
+  attr_accessor :pets
   attr_reader :species
   @@owners = []
 
@@ -23,7 +23,7 @@ class Owner
     self.all.clear
   end
 
-  def species(species)
+  def species()
     @species = species
   end
 
