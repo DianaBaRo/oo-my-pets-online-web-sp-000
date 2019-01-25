978491266a093db7e5fec71e6dfb63e50ea43654
Diana Badas <diana.badas@gmail.com>
2019-01-25 21:55:17 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,7 +5,7 @@ class Owner
   @@owners = []
 
   def initialize(species)
-    @species = "human"
+    @species = species
     @pets = {fishes: [], cats: [], dogs: []}
     self.class.all << self
   end
@@ -31,38 +31,38 @@ class Owner
   end
 
   def pets
-    @pets
+    pets
   end
 
   def buy_fish(name)
     new_fish = Fish.new(name)
-    @pets[:fishes] << new_fish
+    pets[:fishes] << new_fish
   end
 
   def buy_cat(name)
     new_cat = Cat.new(name)
-    @pets[:cats] << new_cat
+    pets[:cats] << new_cat
   end
 
   def buy_dog(name)
     new_dog = Dog.new(name)
-    @pets[:dogs] << new_dog
+    pets[:dogs] << new_dog
   end
 
   def walk_dogs
-    @pets[:dogs].collect { |dog| dog.mood = "happy" }
+    pets[:dogs].collect { |dog| dog.mood = "happy" }
   end
 
   def play_with_cats
-    @pets[:cats].collect { |cat| cat.mood = "happy" }
+    pets[:cats].collect { |cat| cat.mood = "happy" }
   end
 
   def feed_fish
-    @pets[:fishes].collect { |fish| fish.mood = "happy" }
+    pets[:fishes].collect { |fish| fish.mood = "happy" }
   end
 
   def sell_pets
-    @pets.collect do |type, pets|
+    pets.collect do |type, pets|
       pets.collect { |pet| pet.mood = "nervous" }
     end
     pets.clear
